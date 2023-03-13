f761c78c6161cdf55284939b28ee445cd72dae7b
Toogreekz <toogreekmain@gmail.com>
2023-03-13 15:06:31 +0300

    Table and Factors countes

--- main.py
+++ main.py
@@ -38,4 +38,55 @@ for i in s:
     for elem in array[s.index(i)]:
         ws[f"{i}{array[s.index(i)].index(elem) + 2}"] = elem
 
+sigmX = round(x_minus_x_sr[-1] ** 0.5, 5)
+sigmY = round(y_minus_y_sr[-1] ** 0.5, 5)
+sigmOST = round(y_minus_y_r_2[-1] ** 0.5, 5)
+rXY = round(b * sigmX / sigmY, 5)
+R = round((rXY ** 2) * 100, 5)
+elast = round(b * x[-1] / y[-1], 5)
+beta = rXY
+F = round((R / (1 - R)) * (N - 2), 5)
+ma = round(((sigmOST ** 2) * x_2[-2] / N ** 2 * sigmX ** 2) ** 0.5, 5)
+mb = round((sigmOST ** 2 / N * sigmX ** 2) ** 0.5, 5)
+mr = round(((1 - rXY ** 2) / (N - 2)) ** 0.5, 5)
+try:
+    ta = round(a / ma, 5)
+except ZeroDivisionError:
+    ta = "ZeroDivisionException: ma = 0"
+try:
+    tb = round(b / mb, 5)
+except ZeroDivisionError:
+    tb = "ZeroDivisionException: mb = 0"
+try:
+    tr = round(rXY / mr, 5)
+except ZeroDivisionError:
+    tr = "ZeroDivisionException: mr = 0"
+
+ws["N1"] = f"sigmX = {sigmX}"
+ws["N2"] = f"sigmY = {sigmY}"
+ws["N3"] = f"sigmOST = {sigmOST}"
+ws["N4"] = f"rXY = {rXY}"
+ws["N5"] = f"R = {R}"
+ws["N6"] = f"elast = {elast}"
+ws["N7"] = f"beta = {beta}"
+ws["N8"] = f"F = {F}"
+ws["N9"] = f"ma = {ma}"
+ws["N10"] = f"mb = {mb}"
+ws["N11"] = f"mr = {mr}"
+ws["N12"] = f"ta = {ta}"
+ws["N13"] = f"tb = {tb}"
+ws["N14"] = f"tr = {tr}"
+
+if A[-1] <= 10:
+    ws["N16"] = "Точная модель, среднее A <= 10"
+else:
+    ws["N16"] = "Неточная модель, среднее A > 10"
+if rXY >= 0.7:
+    ws["N17"] = "Связь сильная, rXY >= 0.7"
+else:
+    ws["N17"] = "Связь слабая, rXY < 0.7"
+ws["N18"] = f"y зависит от х на {R}"
+ws["N19"] = f"C увелечением x на 1%, у будет увеличиваться на {elast}"
+ws["N20"] = f"С увелечением x на значение {sigmX} y будет увеличиваться на значение {beta}"
+
 wb.save(filename="Econometrics.xlsx")
